3fe693c03fefccb50eb3162fa45c648ce4bfc059
Matt Stjernholm <thew88@gmail.com>
2016-01-04 21:28:32 -0700

    show action for items controller complete

--- test/controllers/api/v1/items_controller_test.rb
+++ test/controllers/api/v1/items_controller_test.rb
@@ -14,11 +14,27 @@ class Api::V1::ItemsControllerTest < ActionController::TestCase
           )
 
    get :index,  :format => :json
-   assert_response :success
 
+   assert_response :success
    assert_equal "I-pod", JSON.parse(response.body).first['name']
    assert_equal "A nice thing that plays your music.", JSON.parse(response.body).first['description']
    assert_equal 10000, JSON.parse(response.body).first['unit_price']
    assert_equal 4, JSON.parse(response.body).first['merchant_id']
  end
+
+ test "should get items show page" do
+  item = Item.create( name: "I-pod",
+               description: "A nice thing that plays your music.",
+               unit_price: 10000,
+               merchant_id: 4
+         )
+
+  get :show,  :format => :json, id: item.id
+
+  assert_response :success
+  assert_equal "I-pod", JSON.parse(response.body)['name']
+  assert_equal "A nice thing that plays your music.", JSON.parse(response.body)['description']
+  assert_equal 10000, JSON.parse(response.body)['unit_price']
+  assert_equal 4, JSON.parse(response.body)['merchant_id']
+end
 end
