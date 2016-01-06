58374a162f55aa7e0e319c8c82dce582e868d7ab
Matt Stjernholm <thew88@gmail.com>
2016-01-05 18:50:03 -0700

    update schema to use decimal for unit_price on items and invoice_items

---
+++ db/migrate/20160106014611_change_invoice_item_unit_price_to_big_decimal.rb
@@ -0,0 +1,5 @@
+class ChangeInvoiceItemUnitPriceToBigDecimal < ActiveRecord::Migration
+  def change
+    change_column :invoice_items, :unit_price, :decimal
+  end
+end
---
+++ db/migrate/20160106014915_change_item_unit_price_to_decimal.rb
@@ -0,0 +1,5 @@
+class ChangeItemUnitPriceToDecimal < ActiveRecord::Migration
+  def change
+    change_column :items, :unit_price, :decimal
+  end
+end
