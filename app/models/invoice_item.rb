8f12e4f7f7c96e00c000951abdff8e65128c24a9
Matt Stjernholm <thew88@gmail.com>
2016-01-05 19:00:22 -0700

    remove before action on invoice_item

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,11 +1,6 @@
 class InvoiceItem < ActiveRecord::Base
   default_scope -> { order('id DESC') }
-  before_create :convert_cents
 
   belongs_to :invoice
   belongs_to :item
-
-  def convert_cents
-    self.unit_price = (unit_price/100).to_f
-  end
 end
