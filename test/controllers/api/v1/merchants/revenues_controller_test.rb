74f150451b64693c60871234bee9d1670a6dab0b
Matt Stjernholm <thew88@gmail.com>
2016-01-06 16:53:23 -0700

    test for single merchant revenue complete

---
+++ test/controllers/api/v1/merchants/revenues_controller_test.rb
@@ -0,0 +1,50 @@
+require 'test_helper'
+
+class Api::V1::Merchants::RevenuesControllerTest < ActionController::TestCase
+  attr_reader :merchant, :customer
+
+  def setup
+    @customer = Customer.create!(first_name: "Matt", last_name: "Smith")
+    @merchant = Merchant.create!(name: "Turing")
+
+    items = 5.times do |i|
+        Item.create( name: "I-pod - #{i}",
+        description: "A nice thing that plays your music. - #{i}",
+        unit_price: 10,
+        merchant_id: merchant.id
+        )
+      end
+
+    invoices =  3.times do |i|
+      Invoice.create!( customer_id: customer.id,
+      merchant_id: merchant.id,
+      status: "shipped"
+      )
+    end
+
+    invoice_items = 3.times do |i|
+        InvoiceItem.create( item_id: Item.find(Item.first.id),
+          invoice_id: invoices[i],
+          quantity: 1,
+          unit_price: 10
+          )
+    end
+
+    transactions = 3.times do |i|
+      Transaction.create( invoice_id: Invoice.first.id,
+        credit_card_number: "1234567#{i}",
+        result: "success")
+    end
+
+    failed_transaction = Transaction.create(invoice_id: Invoice.first.id,
+      credit_card_number: "1234567",
+      result: "failed")
+  end
+
+  test "#show returns the total transactional revenue for given merchant" do
+      get :show, :format => :json, merchant_id: merchant.id
+
+      assert_response :success
+      binding.pry
+  end
+end
--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class MerchantTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+
 end
