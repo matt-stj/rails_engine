5704e0aa1eecc880420f30d0917acc94d62fec09
Matt Stjernholm <thew88@gmail.com>
2016-01-05 22:33:12 -0700

    biz logic in progress

---
+++ app/controllers/api/v1/merchants/revenues_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::Merchants::RevenuesController < ApplicationController
+  respond_to :json
+
+  def show
+    merchant = Merchant.find_by(id: params[:merchant_id])
+    binding.pry
+    #where status = success
+    totals = merchant.invoice_items.map do |invoice_item|
+      invoice_total_price = invoice_item.quantity * invoice_item.item.unit_price.to_f
+    end.inject(0, :+)
+    respond_with totals.round(2)
+  end
+
+successful_transactions =  Merchant.first.transactions.where(result: "success")
+sum_of_unit_prices_on_invoice = successful_transactions.first.invoice.invoice_items.sum(:unit_price).to_f
+
+end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,7 @@ class Merchant < ActiveRecord::Base
 
   has_many :items
   has_many :invoices
+  has_many :invoice_items, through: :invoices
+  has_many :transactions, through: :invoices
+
 end
