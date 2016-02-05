afb01bad47f085d72b05677e196aab1f4a2a0c47
Matt Stjernholm <thew88@gmail.com>
2016-02-04 21:20:09 -0700

    merchants can load revenue with button

--- app/assets/javascripts/load_merchant_revenue.js
+++ app/assets/javascripts/load_merchant_revenue.js
@@ -1,22 +1,22 @@
 function loadMerchantRevenue(){
-  $('#view-merchant-amounts').on('click', function(){
-    $('#view-merchant-amounts').replace(`<div class="ui active inline loader" id="merchants-loader"></div>`)
+  $('.merchants-table').delegate('#view-merchant-amounts', 'click', function() {
+    var $button = $(this).closest('.button')
+    $button.replaceWith(`<div class="ui active centered inline loader" id="merchants-loader"></div>`)
+
+    var merchantId = $(this).attr('data-id')
+    var merchantRow =$(this)
 
     $.ajax({
       type:    "GET",
-      url:     "/api/v1/merchants/most_revenue?quantity=5",
-      success: function(merchants) {
-        $('#merchants-loader').remove()
-
-        $.each(merchants, function(index, merchant) {
-          renderMerchant(merchant)
-        })
-
-        $('#view-merchants').replaceWith(`<button class="ui button floated right blue" id="view-merchant-amounts" style="float: right;">View Total $ Amt.</button>`)
+      url:     "/api/v1/merchants/" + merchantId + "/revenue",
+      success: function(rev_hash) {
+        var formatted_revenue = '$' + rev_hash.revenue.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+        $('#merchants-loader').replaceWith(`<p>${formatted_revenue}</p>`)
       },
       error: function(xhr) {
         console.log(xhr.responseText)
       }
     })
-  })
+})
+
 }
--- app/assets/javascripts/render_merchant.js
+++ app/assets/javascripts/render_merchant.js
@@ -1,3 +1,3 @@
 function renderMerchant(merchant) {
-  $('#top-merchants').append(`<tr><td>${merchant.name}</td><td class='merchant-revenue-column' style="display: none'"><button data-id=${merchant.id} class="mini ui button floated right blue" id="view-merchant-amounts" style="float: right;">$</button></td></tr>`)
+  $('#top-merchants').append(`<tr><td class='merchant'>${merchant.name}</td><td class='merchant-revenue-column' style="display: none'"><button data-id=${merchant.id} class="mini ui button floated right blue" id="view-merchant-amounts" style="float: right;">$</button></td></tr>`)
 }
