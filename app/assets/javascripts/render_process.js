ee11f5bbeac87f8dd1ac2166100655203555b288
Matt Stjernholm <thew88@gmail.com>
2016-02-06 21:37:30 -0700

    remove js backticks

--- app/assets/javascripts/render_process.js
+++ app/assets/javascripts/render_process.js
@@ -1,3 +1,10 @@
 function renderProcess(processObject){
-  $('#processes').append(`<tr><td class='process-name'>${processObject.endpoint}</td><td class='active-record-column'>${processObject.activeRecordQuery}</td><td class='process-time-column'">${processObject.time}</td></tr>`)
+  $('#processes').append("<tr><td class='process-name'>"
+    + processObject.endpoint
+    + "</td><td class='active-record-column'>"
+    + processObject.activeRecordQuery
+    + "</td><td class='process-time-column'>"
+    + processObject.time
+    + "</td></tr>"
+  )
 }
